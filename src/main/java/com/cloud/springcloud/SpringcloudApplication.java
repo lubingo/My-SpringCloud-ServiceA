05a0b0427fe0738050f7b0bfa1d44a827c97c482
lubing <15810172080@139.com>
2019-05-27 17:51:18 +0800

    日志打印优化

--- src/main/java/com/cloud/springcloud/SpringcloudApplication.java
+++ src/main/java/com/cloud/springcloud/SpringcloudApplication.java
@@ -3,7 +3,6 @@ package com.cloud.springcloud;
 import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
