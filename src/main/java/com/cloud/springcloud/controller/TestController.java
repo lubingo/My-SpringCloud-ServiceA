3ea4938e917c4050243b1bfb6a044859f1f22b0b
lubing <15810172080@139.com>
2019-05-30 10:10:04 +0800

    日志打印优化

--- src/main/java/com/cloud/springcloud/controller/TestController.java
+++ src/main/java/com/cloud/springcloud/controller/TestController.java
@@ -1,21 +1,31 @@
 package com.cloud.springcloud.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.servlet.http.HttpServletRequest;
 
+@RefreshScope
 @RestController
 public class TestController {
 
+    private static  final Logger logger = LoggerFactory.getLogger(TestController.class) ;
+    @Value("${configValue}")
+    private  String configValue ;
+
+
     @RequestMapping(value = "/add" ,method = {RequestMethod.GET,RequestMethod.POST})
     public String add(@RequestParam Integer a, @RequestParam Integer b, HttpServletRequest request) {
-        System.out.println(request.getRequestURL());
+        logger.info(request.getRequestURL().toString());
+        logger.info(configValue);
         Integer r = a + b;
-        return "r="+r;
+        return configValue ;//"r="+r;
     }
 
 }
